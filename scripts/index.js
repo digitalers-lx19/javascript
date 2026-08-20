d649b3b7885b66bd296fcd06a94877e6435ca216
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-08-19 22:43:54 -0300

    importar temporizador y metodo asincrono

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 import Gallery from "./Gallery.js";
+import Clock from "./Clock.js";
+import getData from "./xhr.js";
 /** IIFE (Inmediatly Invoked Function Expression) */
 (() => document.addEventListener('DOMContentLoaded', () => {
     Gallery();
-    
+    Clock();
+    getData();
 }))()
