bd95f2d6b37ebf61a1f2adda7bf9bc59cb729d81
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-08-27 14:41:14 -0300

    implementar navegacion para menu y redes sociales

--- app/index.js
+++ app/index.js
@@ -1,4 +1,20 @@
-
+import NavBar from "./components/NavBar.js";
+import { navBarLinks, socialMediaLinks } from "./constants.js";
 (() => document.addEventListener('DOMContentLoaded', () => {
-    
+    const navbar = new NavBar({
+        tag: "nav",
+        links: navBarLinks
+    })
+    navbar.render({
+        classList: "navbar navbar-dark navbar-expand-lg bg-dark",
+        container: root,
+    })
+    const social = new NavBar({
+        tag: "div",
+        links: socialMediaLinks
+    })
+    social.render({
+        classList: "d-flex justify-content-center",
+        container: root,
+    })
 }))()
