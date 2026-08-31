c66a4341c467dc4caeadd780507b254073a33968
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-08-31 18:53:19 -0300

    implementar componente navegacion para redes sociales

---
+++ app/social.js
@@ -0,0 +1,10 @@
+import { socialMediaLinks } from "./constants.js";
+import NavBar from "./components/NavBar.js";
+export const social = new NavBar({
+    tag: "div",
+    links: socialMediaLinks
+});
+social.render({
+    classList: "d-flex justify-content-center gap-2 p-2",
+    container: root,
+});
