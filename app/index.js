78935d563b4d8e4ef8a47171673f932b9fb71d0f
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-08-31 18:54:04 -0300

    importar navegacion y redes sociales

--- app/index.js
+++ app/index.js
@@ -1,20 +1,7 @@
-import NavBar from "./components/NavBar.js";
-import { navBarLinks, socialMediaLinks } from "./constants.js";
+import { navbar } from "./nav.js"
+import { social } from "./social.js";
+
 (() => document.addEventListener('DOMContentLoaded', () => {
-    const navbar = new NavBar({
-        tag: "nav",
-        links: navBarLinks
-    })
-    navbar.render({
-        classList: "navbar navbar-dark navbar-expand-lg bg-dark",
-        container: root,
-    })
-    const social = new NavBar({
-        tag: "div",
-        links: socialMediaLinks
-    })
-    social.render({
-        classList: "d-flex justify-content-center",
-        container: root,
-    })
+    navbar;
+    social;
 }))()
