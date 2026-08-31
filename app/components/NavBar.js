d670d7f0d20dbdbc552a253a71503deff37c0d79
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-08-31 18:51:55 -0300

    actualizar metodo de renderizado para navegacion

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,6 +1,7 @@
 function NavBar({tag, links, childs}){
     this.links = links;
     this.tag = document.createElement(tag);
+    this.childs = childs;
 }
 NavBar.prototype.addEvents = function () {
     this.tag.addEventListener('click', (e) => {
@@ -10,16 +11,18 @@ NavBar.prototype.addEvents = function () {
         }
     })
 }
-NavBar.prototype.render = function ({container, classList, ... props }) {
+NavBar.prototype.render = function ({container = root, classList, linksBox, ... props }) {
+    const box = linksBox ?? this.tag
     Object.assign(this.tag, {
         classList,
         ...props
     })
+    this.childs?.map(item => this.tag.append(item))
     this.links.forEach((link) => {
         if (link.type === "button") 
-            this.tag.innerHTML += `<button class="${link.class}" data-url="${link.url}">${link.text}</button>`
+            box.innerHTML += `<button class="${link.class}" data-url="${link.url}">${link.text}</button>`
         if (link.type === "link")
-            this.tag.innerHTML += `<a class="${link.class}" href="${link.url}" ${link.target ? `target="${link.target}"`: ""}>${link.text}</a>`
+            box.innerHTML += `<a class="${link.class}" href="${link.url}" ${link.target ? `target="${link.target}"`: ""}>${link.text}</a>`
     })
     this.addEvents();
     container.append(this.tag)
