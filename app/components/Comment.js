965d9d8d713998668e3cd604de4a53e56a0af3a0
Cristian Racedo <CristianDRacedo@hotmail.com>
2026-09-01 12:48:52 -0300

    agregar componente comentario

---
+++ app/components/Comment.js
@@ -0,0 +1,23 @@
+export class Comment {
+    constructor({postId, id, name, email, body}){
+        this.postId = postId
+        this.id = id
+        this.name = name
+        this.email = email
+        this.body = body
+    }
+    render(){ return `
+        <article id="p${this.postId}c-${this.id}" class="d-flex align-items-start gap-2">
+            <img class="border border-4 border-dark rounded-circle p-4 | bg-dark bg-gradient" src="" alt="">
+            <div class="card">
+            <address class="card-header">
+                <div class="row | col col-sm-8 col-lg-9">
+                    <a href="mailto:${this.email}">${this.email}</a>
+                    <strong>${this.name}</strong>
+                </div>
+            </address>
+            <div class="card-body">${this.body}</div>
+            </div>
+        </article>
+    `}
+}
